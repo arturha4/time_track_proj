1a84e8b69b0949a9e586b3ee698675ada7decef7
arturdavletshin <sadbooys.2001gmail.com>
2020-12-11 16:55:22 +0500

    11.12.20

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -10,4 +10,12 @@ inline_git_urfu_kb=InlineKeyboardMarkup().add(git_btn,urfu_btn)
 vk_login_yes=InlineKeyboardButton('Да', callback_data='yes')
 vk_login_no=InlineKeyboardButton('Нет', callback_data='no')
 
-inline_vk_kb=InlineKeyboardMarkup().add(vk_login_yes,vk_login_no)
+inline_vk_yes_no_kb=InlineKeyboardMarkup().add(vk_login_yes,vk_login_no)
+
+
+inline_vk_github_kb=InlineKeyboardMarkup().add(vk_btn,git_btn)
+inline_vk_urfu_kb=InlineKeyboardMarkup().add(vk_btn,urfu_btn)
+inline_github_urfu=InlineKeyboardMarkup().add(urfu_btn,git_btn)
+inline_github_kb=InlineKeyboardMarkup().add(git_btn)
+inline_vk_kb=InlineKeyboardMarkup().add(vk_btn)
+inline_urfu_kb=InlineKeyboardMarkup().add(urfu_btn)
