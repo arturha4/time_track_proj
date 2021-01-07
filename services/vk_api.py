2fd4e34e8d8a1cf2e1bad344b1849cd8fa606f40
arturdavletshin <sadbooys.2001gmail.com>
2021-01-08 02:53:32 +0500

    -можно получать данные для трека вк, добавлены скелеты функций для трека вк

--- services/vk_api.py
+++ services/vk_api.py
@@ -1,20 +1,14 @@
 import vk
-import datetime as dt
-
 vk_token = "46e50d8c46e50d8c46e50d8c8a46917fc2446e546e50d8c196fd185478ce57d33254a43"
 session = vk.Session()
 vk_api = vk.API(session)
 
 
-def get_status(id):
+def get_vk_status(id):
     data = vk_api.users.get(v=5.124, user_ids=str(id), fields='online, last_seen', access_token=vk_token)[0]['online']
     return data
 
 
-def vk_track(id):
-    t = dt.datetime.now()
-    while True:
-        delta = dt.datetime.now() - t
-        if delta.seconds >= 5:
-            print(get_status(id))
-            t = dt.datetime.now()
+
+
+
