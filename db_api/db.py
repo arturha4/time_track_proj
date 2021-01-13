53d9c7ccfed0f0302e0ea1a2c2184a50b9308aa9
k1n5-bt <k1n5lobal@gmail.com>
2021-01-14 00:03:17 +0500

    123

--- db_api/db.py
+++ db_api/db.py
@@ -3,6 +3,7 @@ import mysql.connector
 import datetime as dt
 
 from services.vk_api import get_vk_status
+
 db = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -24,7 +25,8 @@ def show_tables():
     for x in cursor:
         print(x)
 
-def create_user(data,tlg_chat_id):
+
+def create_user(data, tlg_chat_id):
     try:
         sql = 'INSERT INTO tlg_bot_user (telegram_id,vk_id,time_in_vk,start_time,end_time,urfu_login,urfu_password,github_login) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
         values = (tlg_chat_id, data['vk_login'], 0, data['start_time'], data['end_time'], data['urfu_login'],
@@ -36,12 +38,9 @@ def create_user(data,tlg_chat_id):
         db.commit()
 
 
-
-
-
 def get_users_data():
     cursor.execute("SELECT * FROM tlg_bot_user")
-    row=cursor.fetchall()
+    row = cursor.fetchall()
     for data in row:
         yield data
 
@@ -49,6 +48,7 @@ def get_users_data():
 def delete_all_users():
     cursor.execute('DELETE FROM tlg_bot_user')
     db.commit()
+
 
 def show_colummns():
     cursor.execute('SHOW COLUMNS FROM tlg_bot_user')
@@ -61,53 +61,52 @@ def get_users_tel_id():
     for i in cursor.fetchall():
         yield i[0]
 
+
 '''
 брать текущее время и сравнивать с стартом и концом в бд,
 запрашивать гет_статус вк и если пользователь онлайн добавлять время в сети вк
 '''
 
+
 def add_vk_time(vk_login):
     try:
-        sql="UPDATE tlg_bot_user SET time_in_vk=time_in_vk+5 WHERE vk_id='{}'".format(vk_login)
-        data=(vk_login)
-        cursor.execute(sql,data)
+        sql = "UPDATE tlg_bot_user SET time_in_vk=time_in_vk+5 WHERE vk_id='{}'".format(vk_login)
+        data = (vk_login)
+        cursor.execute(sql, data)
     except Exception as e:
         print(e)
     finally:
         db.commit()
 
 
-async def update_db(func):
-    data =  get_vk_track_info()
-    str_time_now=dt.datetime.now().strftime('%H:%M')
-    delta= dt.timedelta(minutes=6)
+async def update_db(func, urfuStat, gitStat):
+    data = get_vk_track_info()
+    str_time_now = dt.datetime.now().strftime('%H:%M')
+    delta = dt.timedelta(minutes=6)
     for user in data:
-        user_end_time=user['end_time']
+        user_end_time = user['end_time']
         dt_end_time = dt.datetime.strptime(user_end_time, '%H:%M')
-        if str_time_now > user['start_time'] and user_end_time > str_time_now and get_vk_status(user['vk_id']) == 1:
+        if user['vk_id'] != 0 and user['start_time'] < str_time_now < user_end_time and get_vk_status(user['vk_id']) == 1:
             add_vk_time(user['vk_id'])
-        if str_time_now>=user_end_time and str_time_now<=(dt_end_time+delta).strftime('%H:%M'):
+        if user_end_time <= str_time_now <= (dt_end_time + delta).strftime('%H:%M'):
             await func(user['telegram_id'])
-
-
-
-
+            await urfuStat(user['urfu_login'], user['urfu_password'], user['telegram_id'])
+            await gitStat(user['github_login'], user['telegram_id'])
 
 def get_vk_track_info():
-    cursor.execute('SELECT vk_id,start_time,end_time,telegram_id FROM tlg_bot_user')
+    cursor.execute('SELECT vk_id,start_time,end_time,telegram_id,urfu_login,urfu_password,github_login FROM tlg_bot_user')
     row = cursor.fetchall()
-    l=[{'vk_id':item[0],'start_time':item[1],'end_time':item[2],'telegram_id':item[3]} for item in row if item[0]!='0']
+    l = [{'vk_id': item[0], 'start_time': item[1], 'end_time': item[2], 'telegram_id': item[3], 'urfu_login': item[4], 'urfu_password': item[5], 'github_login': item[6]} for item in row]
     return l
-
 
 
 async def get_user_info(tel_id):
     try:
         cursor.execute('SELECT * FROM tlg_bot_user WHERE telegram_id={}'.format(tel_id))
         row = cursor.fetchone()
-        d={
-            'telegram_id':row[0],
-            'start_time':row[3],
+        d = {
+            'telegram_id': row[0],
+            'start_time': row[3],
             'end_time': row[4],
         }
         return d
--- questions/Test.py
+++ questions/Test.py
@@ -1,16 +1,16 @@
-from aiogram.dispatcher.filters.state import StatesGroup,State
+from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
 class Test(StatesGroup):
-    SET_SERVICE=State()
-    TAKE_VK_ID=State()
-    VK_END=State()
-    VK_YES=State()
-    VK_NO=State()
-    TAKE_URFU_LOGIN=State()
-    TAKE_URFU_PASSWORD=State()
-    TAKE_GITHUB_LOGIN=State()
-    DEFAULT=State()
-    SET_START_TIME=State()
-    SET_END_TIME=State()
-    REGISTERED=State()
+    SET_SERVICE = State()
+    TAKE_VK_ID = State()
+    VK_END = State()
+    VK_YES = State()
+    VK_NO = State()
+    TAKE_URFU_LOGIN = State()
+    TAKE_URFU_PASSWORD = State()
+    TAKE_GITHUB_LOGIN = State()
+    DEFAULT = State()
+    SET_START_TIME = State()
+    SET_END_TIME = State()
+    REGISTERED = State()
